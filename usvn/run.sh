5f29bc06d5d01350c59f61b4fa6f3591341b6e6c
xiagw <fxiaxiaoyu@gmail.com>
2022-06-22 06:47:15 +0800

    remove svn update

--- usvn/run.sh
+++ usvn/run.sh
@@ -21,25 +21,7 @@ _start_lsyncd() {
     ## start lsyncd
     conf_lsyncd=~/.ssh/lsyncd.conf
     if [ -f $conf_lsyncd ]; then
-        lsyncd $conf_lsyncd
-    fi
-}
-
-_svn_notify() {
-    export LANG='en_US.UTF-8'
-    export LC_CTYPE='en_US.UTF-8'
-    export LC_ALL='en_US.UTF-8'
-    svn_need_update=/tmp/svn_need_update
-    [ -f /root/.ssh/svn_auth ] && source /root/.ssh/svn_auth
-    if [ -f $svn_need_update ]; then
-        while read -r line; do
-            /usr/bin/svn update \
-                --username "${svn_user:-root}" \
-                --password "${svn_pass:?empty var}" \
-                --no-auth-cache -N "$line"
-            chown -R 1000.1000 "$line"
-        done <$svn_need_update ## post-commit
-        rm -f $svn_need_update
+        lsyncd -nodaemon $conf_lsyncd
     fi
 }
 
@@ -53,10 +35,10 @@ main() {
 
     _start_lsyncd
 
-    while true; do
-        _svn_notify >/dev/null 2>&1 &
-        sleep 10
-    done &
+    ## svn update /svn_checkout/
+    if [ -f ~/.ssh/svn_update.sh ]; then
+        bash ~/.ssh/svn_update.sh
+    fi
 
     ## start apache
     apache2-foreground
