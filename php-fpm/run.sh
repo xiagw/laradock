38b0e866666bacc241391adcba5d4617aa3fff7b
xiagw <fxiaxiaoyu@gmail.com>
2023-05-10 11:21:04 +0800

    fix phpf-pm run.sh

--- php-fpm/run.sh
+++ php-fpm/run.sh
@@ -13,8 +13,26 @@ for i in /usr/sbin/php-fpm*; do
     [ -x "$i" ] && $i
 done
 
-## start task
-[ -f /var/www/schedule.sh ] && bash /var/www/schedule.sh &
+pre_path=/var/www
+html_path=$pre_path/html
+## create runtime for ThinkPHP
+while true; do
+    for dir in $html_path/ $html_path/tp/ $html_path/tp/*/; do
+        [ -d $dir ] || continue
+        if [[ -f "$dir"/think && -d $dir/thinkphp && -d $dir/application ]]; then
+            run_dir="$dir/runtime"
+            [[ -d "$run_dir" ]] || mkdir "$run_dir"
+            [[ "$(stat -t -c %u $run_dir)" == 33 ]] || chown -R 33:33 "$run_dir"
+        fi
+    done
+    sleep 60
+done &
+## schedule task
+for file in $pre_path/*/task.sh; do
+    if [[ -f $file ]]; then
+        cd "${file##*/}" && bash $file
+    fi
+done &
 
 if nginx -t &>/dev/null; then
     ## start nginx
