ede8736a2e40d5d74386871156ed9756b92cdb84
xiagw <fxiaxiaoyu@gmail.com>
2023-05-13 12:09:55 +0800

    add php base

---
+++ php-fpm/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+main() {
+    me_name="$(basename "$0")"
+    me_path="$(dirname "$(readlink -f "$0")")"
+    me_log="${me_path}/${me_name}.log"
+
+    build_opt="docker build"
+
+    while [[ "${#}" -ge 0 ]]; do
+        case $1 in
+        china | cn)
+            build_opt="$build_opt --build-arg CHANGE_SOURCE=true"
+            ;;
+        8.0 | 8.1)
+            os_ver=22.04
+            php_ver=$1
+            break
+            ;;
+        5.6 | 7.1 | 7.2 | 7.4)
+            os_ver=20.04
+            php_ver=$1
+            break
+            ;;
+        *)
+            os_ver=20.04
+            php_ver=7.1
+            break
+            ;;
+        esac
+        shift
+    done
+
+    build_opt="$build_opt --build-arg UBUNTU_VERSION=$os_ver --build-arg LARADOCK_PHP_VERSION=$php_ver"
+    image_tag_base=deploy/php:${php_ver}-base
+    image_tag=deploy/php:${php_ver}
+    file_url=https://gitee.com/xiagw/laradock/raw/in-china/php-fpm
+    file_base=Dockerfile.php-base
+    ## php base image ready?
+    cd $me_path || exit 1
+    if ! docker images | grep -q "$image_tag_base"; then
+        if [[ ! -f $file_base ]]; then
+            curl -fsSLO $file_url/$file_base
+        fi
+        $build_opt -t $image_tag_base -f $file_base . || return 1
+    fi
+    ## build php image
+    echo "FROM $image_tag_base" >Dockerfile
+    [[ -f root ]] || mkdir root
+    $build_opt -t $image_tag -f Dockerfile .
+}
+
+main "$@"
