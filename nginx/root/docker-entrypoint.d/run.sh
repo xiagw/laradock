dfb90a1a9834567931d0bc218d17d0cd4e61c7ac
xiagw <fxiaxiaoyu@gmail.com>
2023-05-18 12:09:38 +0800

    add nginx dockerfile.base

---
+++ nginx/root/docker-entrypoint.d/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+_default_key() {
+    ssl_dir="/etc/nginx/conf.d/ssl"
+    default_key="$ssl_dir/default.key"
+    default_csr="$ssl_dir/default.csr"
+    default_crt="$ssl_dir/default.crt"
+    dhparams=$ssl_dir/dhparams.pem
+
+    [ -d "$ssl_dir" ] || mkdir -p "$ssl_dir"
+
+    if [ ! -f "$default_crt" ]; then
+        openssl genrsa -out "$default_key" 2048
+        openssl req -new -key "$default_key" -out "$default_csr" -subj "/CN=default/O=default/C=UK"
+        openssl x509 -req -days 365 -in "$default_csr" -signkey "$default_key" -out "$default_crt"
+    fi
+
+    if [ ! -f $dhparams ]; then
+        openssl dhparam -dsaparam -out $dhparams 4096
+    fi
+
+    chown nginx:nginx $ssl_dir/*.key /var/log/nginx
+    chmod 600 $ssl_dir/*.key
+}
+
+_default_key &
+
+html_dir=/var/www/html
+[ -d $html_dir/.well-known/acme-challenge ] || mkdir -p $html_dir/.well-known/acme-challenge
+[ -f $html_dir/index.html ] || date >>$html_dir/index.html
+# chown -R 1000:1000 $html_dir
+
+## nginx 4xx 5xx
+if [ ! -f $html_dir/4xx.html ]; then
+    echo 'Error page: 4xx' >>$html_dir/4xx.html
+fi
+if [ ! -f $html_dir/5xx.html ]; then
+    echo 'Error page: 5xx' >>$html_dir/5xx.html
+fi
---
+++ nginx/root/opt/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -xe
+
+if [ "${CHANGE_SOURCE}" = true ]; then
+    sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/' /etc/apk/repositories
+fi
+
+apk update
+apk upgrade
+apk add --no-cache openssl bash curl
+touch /var/log/messages
+
+echo http://dl-2.alpinelinux.org/alpine/edge/community/ >> /etc/apk/repositories
+apk --no-cache add shadow
+groupmod -g 1000 www-data
+adduser -u 1000 -D -S -G www-data www-data
+
+# Set upstream conf and remove the default conf
+echo "upstream php-upstream { server ${PHP_UPSTREAM_CONTAINER}:${PHP_UPSTREAM_PORT}; }" >/etc/nginx/php-upstream.conf
+# rm /etc/nginx/conf.d/default.conf
+
+sed -i 's/\r//g' /docker-entrypoint.d/run.sh
+chmod +x /docker-entrypoint.d/run.sh
+
