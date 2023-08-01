484bc8986fcf652a62bbbbee06c7ddf54b4d7e27
xiagw <fxiaxiaoyu@gmail.com>
2023-08-01 21:41:58 +0800

    WIP usvn run.sh

--- usvn/root/opt/run.sh
+++ usvn/root/opt/run.sh
@@ -4,22 +4,6 @@ _log() {
     echo "[$(date +%F_%T)], $*" >>"$me_log"
 }
 
-_generate_ssh_key() {
-    ## generate ssh key
-    [ -f "$HOME"/.ssh/id_ed25519 ] && return
-    mkdir -m 700 "$HOME"/.ssh
-    ssh-keygen -t ed25519 -C "root@usvn.docker" -N '' -f "$HOME"/.ssh/id_ed25519
-    (
-        echo 'Host *'
-        echo "IdentityFile \$HOME/.ssh/id_ed25519"
-        echo 'StrictHostKeyChecking no'
-        echo 'GSSAPIAuthentication no'
-        echo 'Compression yes'
-    ) >"$HOME"/.ssh/config
-    chmod 600 "$HOME"/.ssh/config
-    # cat $HOME.ssh/id_ed25519.pub
-}
-
 _schedule_svn_update() {
     ## UTC time
     while [[ "$(date +%H%M)" == 2005 ]]; do
@@ -33,40 +17,39 @@ _schedule_svn_update() {
     done
 }
 
-_chown_chmod() {
-    args="$1"
-    ## ThinkPHP, crate folder runtime / 创建文件夹 runtime
-    if [[ "$args" =~ (application) ]]; then
-        path_app="${args%/application/*}"
-        if [[ -f "$path_app"/.env && -d "$path_app"/vendor && ! -d "$path_app"/runtime ]]; then
-            mkdir "$path_app"/runtime
-            chown 33:33 "$path_app"/runtime
-            chmod 755 "$path_app"/runtime
-        fi
-    fi
-    ## chmod
-    if [[ -d "$args" ]]; then
-        chmod 755 "$args"
-    elif [[ -f "$args" ]]; then
-        chmod 644 "$args"
-    fi
-    ## chown
-    if [[ "$args" =~ (runtime) ]]; then
-        chown 33:33 "${args%/runtime/*}"/runtime
-    elif [[ "$args" =~ (Runtime) ]]; then
-        chown 33:33 "${args%/Runtime/*}"/Runtime
-    else
-        chown 0:0 "$args"
-    fi
-}
-
 _kill() {
     echo "receive SIGTERM, kill ${pids[*]}"
-    rm -f "$me_lock"
     for pid in "${pids[@]}"; do
         kill "$pid"
         wait "$pid"
     done
+}
+
+_watch_new() {
+    inotifywait -mqr -e create --exclude '/db/transactions/|/db/txn-protorevs/' ${path_svn_pre}/ |
+        while read -r path action file; do
+            echo "${path}${action}${file}" | grep -P '/db/revprops/\d+/CREATE\d+' || continue
+            ## get $repo_name
+            repo_name=${path#"${path_svn_pre}"/}
+            repo_name=${repo_name%%/*}
+            if [[ ! -d "$path_svn_pre/${repo_name:-none}" ]]; then
+                _log "not found repo: ${repo_name:-none}"
+                continue
+            fi
+            ## svnlook dirs-change
+            sleep 2 ## because "inotifywait" is too fast, so need to wait until "svn" write to disk
+            for dir_changed in $($bin_svnlook dirs-changed -r "${file}" "$path_svn_pre/${repo_name}"); do
+                _log "svnlook dirs-changed: $dir_changed"
+                ## not found svn repo in $path_svn_checkout, then svn checkout
+                if [ ! -d "$path_svn_checkout/$repo_name/.svn" ]; then
+                    $bin_svn checkout "file://$path_svn_pre/$repo_name" "$path_svn_checkout/$repo_name"
+                fi
+                ## svn update
+                echo "${dir_changed}" | grep runtime >>"$me_path"/runtime.log
+                $bin_svn update "$path_svn_checkout/$repo_name/${dir_changed}"
+                _chown_chmod "$path_svn_checkout/$repo_name/${dir_changed}"
+            done
+        done
 }
 
 main() {
@@ -77,13 +60,6 @@ main() {
     me_name="$(basename "$0")"
     me_path="$(dirname "$(readlink -f "$0")")"
     me_log="${me_path}/${me_name}.log"
-    me_lock=/tmp/${me_name}.lock
-
-    if [ -e "$me_lock" ] && kill -0 "$(cat "$me_lock")"; then
-        echo "Already running, exit"
-        exit 1
-    fi
-    echo $$ >"${me_lock}"
 
     path_www=/var/www
     path_usvn=$path_www/usvn
@@ -96,28 +72,18 @@ main() {
         mkdir -p $path_svn_pre
     fi
     chown -R 33:33 $path_usvn/{config,files}
-    ## web app usvn
     if [[ ! -d $path_usvn/public ]]; then
         rsync -a ${path_usvn}_src/ $path_usvn/
     fi
 
     ## schedule svn cleanup/update root dirs
     _schedule_svn_update &
-    ## ssh key
-    _generate_ssh_key
 
-    ## lsyncd /root/tool/lsyncd.conf
-    # if [ -f /etc/lsyncd/lsyncd.conf.lua ]; then
-    #     lsyncd /etc/lsyncd/lsyncd.conf.lua &
-    # fi
-
-    # exec &> >(tee -a "$me_log")
     ## 识别中断信号，停止 java 进程
     trap _kill HUP INT PIPE QUIT TERM
     pids=()
-    ## 识别中断信号，停止 java 进程
-    trap _kill HUP INT PIPE QUIT TERM
-    svnserve -d -r $path_svn_pre
+    _watch_new
+    # svnserve -d -r $path_svn_pre
     ## start apache
     apache2-foreground &
     pids+=("$!")
