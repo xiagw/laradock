d7ec1c029209722a42d06ab69abbfaf753ee7be7
xiagw <fxiaxiaoyu@gmail.com>
2023-09-22 17:43:31 +0800

    🐞fix(M  nginx/startup.sh)

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -6,26 +6,26 @@ if [ ! -f $ssl_dir/default.pem ]; then
     openssl genrsa -out "$ssl_dir/default.key" 2048
     openssl req -new -key "$ssl_dir/default.key" -out "$ssl_dir/default.csr" -subj "/CN=default/O=default/C=CN"
     openssl x509 -req -days 3650 -in "$ssl_dir/default.csr" -signkey "$ssl_dir/default.key" -out "$ssl_dir/default.pem"
-    chown nginx $ssl_dir/*.key
-    chmod 600 $ssl_dir/*.key
 fi
 
 if [ ! -f $ssl_dir/dhparams.pem ]; then
     openssl dhparam -dsaparam -out $ssl_dir/dhparams.pem 4096
 fi
 
-
 # chmod 0644 /etc/logrotate.d/nginx
 
 html_path=/var/www/html
 log_path=/var/log/nginx
-[ -d $html_path/.well-known/acme-challenge ] || mkdir -p $html_path/.well-known/acme-challenge
+[ -f $html_path/index.html ] || date >>$html_path/index.html
+if [ ! -d $html_path/.well-known/acme-challenge ]; then
+    mkdir -p $html_path/.well-known/acme-challenge
+fi
 if [ ! -d $html_path ]; then
     mkdir -p $html_path
-    chown -R 1000:1000 $html_path
 fi
-chown nginx $log_path
-[ -f $html_path/index.html ] || date >>$html_path/index.html
+chown -R 1000:1000 $html_path
+chmod 600 $ssl_dir/*.key
+chown -R nginx $ssl_dir/*.key $log_path
 
 ## nginx 4xx 5xx
 if [ ! -f $html_path/4xx.html ]; then
