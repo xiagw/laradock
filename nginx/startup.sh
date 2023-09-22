1a4061a56e2959a0031fec07cb3492b7ce10071d
xiagw <fxiaxiaoyu@gmail.com>
2023-09-22 11:45:48 +0800

    🐞fix(M  nginx/Dockerfile M  nginx/sites/default.conf D  nginx/sites/default.inc)

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -5,16 +5,16 @@ ssl_dir="/etc/nginx/conf.d/ssl"
 
 if [ ! -f $ssl_dir/default.crt ]; then
     openssl genrsa -out "$ssl_dir/default.key" 2048
-    openssl req -new -key "$ssl_dir/default.key" -out "$ssl_dir/default.csr" -subj "/CN=default/O=default/C=UK"
-    openssl x509 -req -days 365 -in "$ssl_dir/default.csr" -signkey "$ssl_dir/default.key" -out "$ssl_dir/default.crt"
-    chmod 644 $ssl_dir/default.key
+    openssl req -new -key "$ssl_dir/default.key" -out "$ssl_dir/default.csr" -subj "/CN=default/O=default/C=CN"
+    openssl x509 -req -days 365 -in "$ssl_dir/default.csr" -signkey "$ssl_dir/default.key" -out "$ssl_dir/default.pem"
+    chown nginx $ssl_dir/default.key
 fi
 
 if [ ! -f $ssl_dir/dhparams.pem ]; then
     openssl dhparam -dsaparam -out $ssl_dir/dhparams.pem 4096
 fi
 
-chmod 0644 /etc/logrotate.d/nginx
+# chmod 0644 /etc/logrotate.d/nginx
 chown 1000:0 /var/log/nginx
 
 ## /var/www/html
@@ -33,8 +33,14 @@ if [ ! -f /var/www/html/5xx.html ]; then
     echo 'Error page: 5xx' >>/var/www/html/5xx.html
 fi
 
+## remove log files / 自动清理超过7天的旧日志文件
+while [ -d /var/log/nginx ]; do
+    find /var/log/nginx -type f -iname '*.log' -ctime +15 -print0 | xargs -t -0 rm -f
+    sleep 1d
+done &
+
 # Start crond in background
 crond -l 2 -b
 
 # Start nginx in foreground
-exec nginx
+# exec nginx
