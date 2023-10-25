446758b104d6263634b6137d1a174d81e8d024e6
xiagw <fxiaxiaoyu@gmail.com>
2023-10-25 14:45:27 +0800

    fix nodejs

--- nodejs/run.sh
+++ nodejs/run.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/bash
 
+_kill() {
+    _msg "receive SIGTERM, kill ${pids[*]}"
+    for pid in "${pids[@]}"; do
+        kill "$pid"
+        wait "$pid"
+    done
+}
+
+trap _kill HUP INT PIPE QUIT TERM
+
+pids=()
+
 if [ -d ./src ]; then
     echo "found ./src"
     start_cmd=$(awk '/"start".*\.js/ {gsub(/"/,""); gsub(/start:/,""); gsub(/^\s+/,""); print $0}' nodejs/package.json)
-    eval $start_cmd
+    eval $start_cmd &
 else
     echo "not found ./src"
-    tail -f package.json
+    tail -f package.json &
 fi
+
+pids+=("$!")
+
+wait
