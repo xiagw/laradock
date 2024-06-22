1995991a863a82088cb9c1f7ef257da20f6716fd
xiagw <fxiaxiaoyu@gmail.com>
2024-06-23 00:13:11 +0800

    🐞fix(M  docker-compose.override.yml M  percona-xtrabackup/Dockerfile M  percona-xtrabackup/root/opt/run.sh)

--- percona-xtrabackup/root/opt/run.sh
+++ percona-xtrabackup/root/opt/run.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
 set -xe
-/usr/bin/xtrabackup --backup \
-    --datadir=/var/lib/mysql/ \
-    --target-dir=/backup/xtrabackup \
-    --host=mysql \
-    --user=root \
-    --password=${MYSQL_ROOT_PASSWORD}
+if [ -f /backup/xtrabackup/xtrabackup_logfile ]; then
+    /usr/bin/xtrabackup \
+        --backup \
+        --target-dir=/backup/inc-"$(date +%s)" \
+        --incremental-basedir=/backup/xtrabackup \
+        --host=mysql \
+        --user=root \
+        --password="${MYSQL_ROOT_PASSWORD}"
+else
+    /usr/bin/xtrabackup --backup \
+        --datadir=/var/lib/mysql/ \
+        --target-dir=/backup/xtrabackup \
+        --host=mysql \
+        --user=root \
+        --password="${MYSQL_ROOT_PASSWORD-}"
+fi
