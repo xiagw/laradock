c8473e802239d715f18864c2ef8be1e660f4d1ab
xiagw <fxiaxiaoyu@gmail.com>
2024-06-23 11:19:31 +0800

    🐞fix(M  percona-xtrabackup/root/opt/run.sh)

--- percona-xtrabackup/root/opt/run.sh
+++ percona-xtrabackup/root/opt/run.sh
@@ -2,26 +2,32 @@
 
 set -xe
 
-backup_bin=/usr/bin/xtrabackup
-backup_path=/backup/xtrabackup
-backup_base="$backup_path/base-$(date +%U)"
+while true; do
+    backup_bin=/usr/bin/xtrabackup
+    backup_path=/backup/xtrabackup
+    backup_base="$backup_path/base-$(date +%U)"
 
-if [ -f "$backup_base"/xtrabackup_logfile ]; then
-    $backup_bin \
-        --backup \
-        --host=mysql \
-        --user=root \
-        --password="${MYSQL_ROOT_PASSWORD-}" \
-        --target-dir="$backup_path"/inc-"$(date +%s)" \
-        --incremental-basedir="$backup_base"
-else
-    [ -d "$backup_base" ] || mkdir -p "$backup_base"
-    # chown 999:999 "$backup_base"
-    $backup_bin \
-        --backup \
-        --datadir=/var/lib/mysql/ \
-        --host=mysql \
-        --user=root \
-        --password="${MYSQL_ROOT_PASSWORD-}" \
-        --target-dir="$backup_base"
-fi
+    if [ -f "$backup_base"/xtrabackup_logfile ]; then
+        echo "$(date), backup incremental...begin"
+        $backup_bin \
+            --backup \
+            --host=mysql \
+            --user=root \
+            --password="${MYSQL_ROOT_PASSWORD-}" \
+            --target-dir="$backup_path"/inc-"$(date +%s)" \
+            --incremental-basedir="$backup_base"
+        echo "$(date), backup incremental end."
+    else
+        echo "$(date), backup full...begin"
+        $backup_bin \
+            --backup \
+            --datadir=/var/lib/mysql/ \
+            --host=mysql \
+            --user=root \
+            --password="${MYSQL_ROOT_PASSWORD-}" \
+            --target-dir="$backup_base"
+        echo "$(date), backup full end."
+    fi
+done &
+
+wait
