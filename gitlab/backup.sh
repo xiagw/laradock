7ddc7007de7668f2d724da0da261b6b379fc6f19
xiagw <fxiaxiaoyu@gmail.com>
2024-06-29 15:14:19 +0800

    🐞fix(M  gitlab/backup.sh)

--- gitlab/backup.sh
+++ gitlab/backup.sh
@@ -4,5 +4,5 @@
 set -xe
 me_path="$(dirname "$(readlink -f "$0")")"
 cd "${me_path}/.."
-docker compose gitlab gitlab-backup create SKIP=uploads,builds,artifacts,lfs,terraform_state,registry,repositories,packages
+docker compose exec gitlab gitlab-backup create SKIP=uploads,builds,artifacts,lfs,terraform_state,registry,repositories,packages
 
