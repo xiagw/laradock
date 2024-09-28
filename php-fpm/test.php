4289afbd777b5ddfb0ecb29ebba7f14992cc287d
xiagw <fxiaxiaoyu@gmail.com>
2024-09-28 18:32:00 +0800

    fix test.php

--- php-fpm/test.php
+++ php-fpm/test.php
@@ -1,33 +1,57 @@
 <?php
 
-$redis = new Redis();
-$response = 'default value';
-$redis->connect('redis',6379);
-$redis->auth('ENV_REDIS_PASSWORD');
-$redis->set('foo', rand(1,1000000));
-$response = $redis->get('foo');
-echo "Test Redis ... </br>\n";
-echo "Redis key foo(random): $response </br>\n";
 
-$username = "ENV_MYSQL_USER";
-$passwd = "ENV_MYSQL_PASSWORD";
-$arr_dns = array("mysql");
-if (!defined('MYSQLI_OPT_READ_TIMEOUT')) {
-    define ('MYSQLI_OPT_READ_TIMEOUT', 5);
-}
-// PHP 7.4 以上支持 MySQL 8.*，否则不支持
-echo "Test MySQL ... </br>\n";
-foreach ($arr_dns as $dns) {
-    $connection = mysqli_init();
-    $connection->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
-    $connection->options(MYSQLI_OPT_READ_TIMEOUT, 5);
-    // $connection = mysqli_connect($dns,$username,$passwd);
-    if(mysqli_real_connect($connection,$dns,$username,$passwd)){
-        echo "Connect mysql:    OK. </br>\n";
-    } else {
-        // die("connection Failed:" . mysqli_connect_errno());
-        echo "Connect mysql:    Fail. </br>\n";
+class Sample {
+
+    public static function testPhp(){
+        $display_info="Test PHP Page...";
+        echo "$display_info";
     }
-    mysqli_close($connection);
+
+    public static function testRedis(){
+        $redis = new Redis();
+        $response = 'default value';
+        $redis->connect('redis',6379);
+        $redis->auth('ENV_REDIS_PASSWORD');
+        $redis->set('foo', rand(1,1000000));
+        $response = $redis->get('foo');
+        echo "Test Redis ... </br>\n";
+        echo "Redis key foo(random): $response </br>\n";
+    }
+
+    public static function testMysql(){
+        $username = "ENV_MYSQL_USER";
+        $passwd = "ENV_MYSQL_PASSWORD";
+        $arr_dns = array("mysql");
+        if (!defined('MYSQLI_OPT_READ_TIMEOUT')) {
+            define ('MYSQLI_OPT_READ_TIMEOUT', 5);
+        }
+        // PHP 7.4 以上支持 MySQL 8.*，否则不支持
+        echo "Test MySQL ... </br>\n";
+        foreach ($arr_dns as $dns) {
+            $connection = mysqli_init();
+            $connection->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+            $connection->options(MYSQLI_OPT_READ_TIMEOUT, 5);
+            // $connection = mysqli_connect($dns,$username,$passwd);
+            if(mysqli_real_connect($connection,$dns,$username,$passwd)){
+                echo "Connect mysql:    OK. </br>\n";
+            } else {
+                // die("connection Failed:" . mysqli_connect_errno());
+                echo "Connect mysql:    Fail. </br>\n";
+            }
+            mysqli_close($connection);
+        }
+    }
+
+    public static function main(){
+
+        self::testPhp();
+
+        self::testRedis();
+
+        self::testMysql();
+    }
 }
+
+Sample::main();
 
