e0e117af6855c95396236cd6222f5edcba450006
xiagw <fxiaxiaoyu@gmail.com>
2024-11-13 16:49:51 +0800

    feat: 优化nginx配置和mysql权限 - nginx配置增加http2支持 - nginx配置增加安全防护 - mysql配置增加日志表权限

--- mysql/docker-entrypoint-initdb.d/fix.sh
+++ mysql/docker-entrypoint-initdb.d/fix.sh
@@ -2,3 +2,23 @@
 
 printf "[client]\npassword=%s\n" "${MYSQL_ROOT_PASSWORD}" >/root/.my.cnf
 chmod 600 /root/.my.cnf
+
+grant2table() {
+    tmp_sql=$(mktemp)
+    mysql defaultdb -Ne 'show tables;' | while read -r line; do
+        case "$line" in
+        transportation_ad_log | transportation_admin_log | transportation_gold_log)
+            echo "GRANT SELECT, INSERT ON defaultdb.$line TO 'defaultdb'@'%';"
+            ;;
+        *_loggg)
+            echo "GRANT SELECT, INSERT ON defaultdb.$line TO 'defaultdb'@'%';"
+            ;;
+        *)
+            echo "GRANT ALL PRIVILEGES ON defaultdb.$line TO 'defaultdb'@'%';"
+            ;;
+        esac
+    done >"$tmp_sql"
+
+    mysql -v <"$tmp_sql"
+    rm -f "$tmp_sql"
+}
