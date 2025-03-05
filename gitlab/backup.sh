2b879cde2a3b7af17b63690109fd736fc9f24c5c
xiagw <fxiaxiaoyu@gmail.com>
2025-03-05 14:20:57 +0800

    feat(gitlab): Enhance backup and Nginx configuration scripts
    - Refactor backup.sh to use more robust path and domain handling
    - Add Gitea upstream configuration in nginx.ext.conf
    - Improve SSL and Nginx configuration management
    - Simplify docker compose command execution

--- gitlab/backup.sh
+++ gitlab/backup.sh
@@ -2,27 +2,30 @@
 
 set -xe
 me_path="$(dirname "$(readlink -f "$0")")"
-laradock_path="${me_path}/.."
-source <(grep '^GITLAB_DOMAIN_NAME_GIT' "${laradock_path}/.env")
-domain=${GITLAB_DOMAIN_NAME_GIT#git.}
+laradock_path="$(dirname "${me_path}")"
+laradock_data="$(dirname "${laradock_path}")/laradock-data"
+domain_git=$(grep '^GITLAB_DOMAIN_NAME_GIT' "${laradock_path}/.env" | cut -d'=' -f2)
+domain_base=${domain_git#git.}
+
 cd "$laradock_path"
 
 case "$1" in
 -s | --ssl)
-    gitlab_ssl_path="${laradock_path}/../../laradock-data/gitlab/config/ssl"
+    gitlab_ssl_path="${laradock_data}/gitlab/config/ssl"
     cert_path=$HOME/.acme.sh/dest
 
     [ -d "$gitlab_ssl_path" ] || sudo mkdir -p "$gitlab_ssl_path"
-    openssl x509 -enddate -noout -in "$cert_path/${domain}".pem
-    ls -al "$cert_path/${domain}".* "$gitlab_ssl_path/${GITLAB_DOMAIN_NAME_GIT}".*
-    sudo cp "$cert_path/${domain}".key "$gitlab_ssl_path/${GITLAB_DOMAIN_NAME_GIT}".key
-    sudo cp "$cert_path/${domain}".pem "$gitlab_ssl_path/${GITLAB_DOMAIN_NAME_GIT}".crt
-    echo "  docker compose exec gitlab bash -c 'gitlab-ctl restart nginx'"
+    openssl x509 -enddate -noout -in "$cert_path/${domain_base}".pem
+    sudo ls -al "$cert_path/${domain_base}".* "$gitlab_ssl_path/${domain_git}".*
+    sudo cp "$cert_path/${domain_base}".key "$gitlab_ssl_path/${domain_git}".key
+    sudo cp "$cert_path/${domain_base}".pem "$gitlab_ssl_path/${domain_git}".crt
+    docker compose exec gitlab "gitlab-ctl restart nginx"
     ;;
 -n | --nginx)
-    gitlab_nginx_conf="${laradock_path}/../../laradock-data/gitlab/data/nginx/conf"
+    gitlab_nginx_conf="${laradock_data}/gitlab/data/nginx/conf"
     sudo cp -vf "${me_path}/nginx.ext.conf" "$gitlab_nginx_conf/nginx.ext.conf"
-    sudo sed -i -e "s/example.com/$domain/g" "$gitlab_nginx_conf/nginx.ext.conf"
+    sudo sed -i -e "s/example.com/$domain_base/g" "$gitlab_nginx_conf/nginx.ext.conf"
+    docker compose exec gitlab "gitlab-ctl restart nginx"
     ;;
 *)
     docker compose exec gitlab gitlab-backup create SKIP=uploads,builds,artifacts,lfs,terraform_state,registry,repositories,packages
