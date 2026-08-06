4fc6ec37b51e407dda130442141b5b6486975a86
xiagw <fxiaxiaoyu@gmail.com>
2026-08-06 13:59:02 +0800

    fix: update offline installation script to load laradock images from specified directory

--- fly.sh
+++ fly.sh
@@ -511,7 +511,8 @@ install_offline() {
     $use_sudo systemctl daemon-reload
     $use_sudo systemctl restart docker.service
 
-    find . -maxdepth 1 -name "laradock*.tar" -type f -print0 | xargs -r -t -0 $use_sudo docker load -i {}
+    # find . -maxdepth 1 -name "laradock*.tar" -type f -print0 | xargs -0 -n1 -I{} $use_sudo docker load -i '{}'
+    find "$offline_dir" -maxdepth 1 -name "laradock*.tar" -type f -print0 -exec $use_sudo docker load -i '{}' +
 
     cd "$g_laradock_path" || exit 1
     docker compose up -d redis mysql php-fpm spring nginx
